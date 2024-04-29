fec9274535e533110f7ec20479bb8a9307ad619e
dkozTA <anhthe45776@gmail.com>
2024-04-29 22:57:17 +0700

    simple search funtion add

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
 const Search = () => {
+  const [products, setProducts] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/api/products');
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.error('Error fetching products', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  useEffect(() => {
+    if (searchValue) {
+      setFilteredProducts(
+        products.filter(product =>
+          product.name.toLowerCase().startsWith(searchValue.toLowerCase())
+        )
+      );
+    } else {
+      setFilteredProducts([]);
+    }
+  }, [searchValue, products]);
+
+  const handleInputChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
   return (
-    <form action="#" className="search">
+    <form action="#" className="search" onSubmit={(e) => e.preventDefault()}>
       <div className="input-group">
         <input
           id="search"
@@ -9,6 +45,7 @@ const Search = () => {
           className="form-control"
           placeholder="Search"
           required
+          onChange={handleInputChange}
         />
         <label className="visually-hidden" htmlFor="search"></label>
         <button
@@ -19,7 +56,11 @@ const Search = () => {
           <i className="bi bi-search"></i>
         </button>
       </div>
+      {filteredProducts.map(product => (
+        <div key={product._id}>{product.name}</div>
+      ))}
     </form>
   );
 };
+
 export default Search;
